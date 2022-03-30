774f6c61098fc95902c39401e3f69917b6bf383a
AlexanderTirik <sasha.tirik@gmail.com>
2022-03-30 23:03:29 +0300

    fix warnings

--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -1,13 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './components/Home';
 import theme from './theme';
 import { ChakraProvider } from '@chakra-ui/react';
 import { Provider } from 'react-redux';
 import { store } from './app/store';
 import { IntlProvider } from 'react-intl';
 import Routing from './scenes/Routing';
-import { BrowserRouter } from 'react-router-dom';
 
 ReactDOM.render(
   <React.StrictMode>
