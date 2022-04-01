4ec29a3734ace37e94938ec0f6deda20f9bd9df2
AlexanderTirik <sasha.tirik@gmail.com>
2022-04-01 22:48:34 +0300

    update config

--- backend/src/config.ts
+++ backend/src/config.ts
@@ -1,4 +1,5 @@
 export const config = {
   accountId: 's6eRUl8CdEowIo2CF3VyEA',
-  blockchainNet: 'testnet',
+  blockchainNet: 'devnet',
+  programId: 'Dgw1WKu8qJg3XR1djgRbmP9cysmjep4TXASHEpe45ZVG',
 };
