95c76f990e5e08f1ebd9becef948a5627252dcb9
AlexanderTirik <sasha.tirik@gmail.com>
2022-04-04 20:25:27 +0300

    update contract

--- contract/programs/opencharitytracker/src/lib.rs
+++ contract/programs/opencharitytracker/src/lib.rs
@@ -7,22 +7,23 @@ pub mod opencharitytracker {
   use super::*;
   pub fn start_open_charity_tracker(ctx: Context<StartOpenCharityTracker>) -> Result<()> {
     let base_account = &mut ctx.accounts.base_account;
-    base_account.sum = 0;
+    base_account.sum = 0.0;
     let owner = *ctx.accounts.user.key;
     base_account.owner = owner;
     Ok(())
   }
 
-  pub fn add_transaction(ctx: Context<AddTransaction>, amount: u64, hash: String) -> Result<()> {
+  pub fn add_transaction(ctx: Context<AddTransaction>, str_amount: String, hash: String) -> Result<()> {
     let owner = *ctx.accounts.user.key;
     let base_account = &mut ctx.accounts.base_account;
+    let amount = str_amount.parse::<f32>().unwrap();
     if base_account.owner != owner {
       return Err(error!(ErrorCode::PermissionDenied));
     }
 
     let tsn = TransactionStruct {
       amount,
-      hash
+      hash: hash.to_string()
     };
 		
     base_account.transactions.push(tsn);
@@ -39,7 +40,7 @@ pub enum ErrorCode {
 
 #[derive(Accounts)]
 pub struct StartOpenCharityTracker<'info> {
-  #[account(init, payer = user, space = 9000)]
+  #[account(init, payer = user, space = 8000)]
   pub base_account: Account<'info, BaseAccount>,
   #[account(mut)]
   pub user: Signer<'info>,
@@ -56,13 +57,13 @@ pub struct AddTransaction<'info> {
 
 #[derive(Debug, Clone, AnchorSerialize, AnchorDeserialize)]
 pub struct TransactionStruct {
-    pub amount: u64,
+    pub amount: f32,
     pub hash: String,
 }
 
 #[account]
 pub struct BaseAccount {
     pub owner: Pubkey,
-    pub sum: u64,
+    pub sum: f32,
     pub transactions: Vec<TransactionStruct>,
 }
--- contract/tests/opencharitytracker.ts
+++ contract/tests/opencharitytracker.ts
@@ -20,29 +20,27 @@ describe("opencharitytracker", () => {
       systemProgram: SystemProgram.programId,
     }).signers([baseAccount]).rpc();
     const account = await program.account.baseAccount.fetch(baseAccount.publicKey);
-    assert.equal(account.sum.toNumber(), 0)
+    assert.equal(account.sum, 0);
   });
 
   it("Add transaction", async () => {
-    const account1 = await program.account.baseAccount.fetch(baseAccount.publicKey);
-    console.log(account1.transactions);
-    await program.methods.addTransaction(new anchor.BN("10"), "some hash").accounts({
+    await program.methods.addTransaction("10", "some hash").accounts({
         baseAccount: baseAccount.publicKey,
         user: provider.wallet.publicKey
       }).rpc();
 
     let account = await program.account.baseAccount.fetch(baseAccount.publicKey);
 
-    assert.equal(account.sum.toNumber(), 10)
+    assert.equal(account.sum, 10)
 
-    await program.methods.addTransaction(new anchor.BN("1"), "some hash").accounts({
+    await program.methods.addTransaction("1.5", "some hash").accounts({
         baseAccount: baseAccount.publicKey,
         user: provider.wallet.publicKey
     }).rpc();
 
     account = await program.account.baseAccount.fetch(baseAccount.publicKey);
 
-    assert.equal(account.sum.toNumber(), 11)
+    assert.equal(account.sum, 11.5)
     assert.equal((account.transactions as any[]).length, 2)
   });
 
